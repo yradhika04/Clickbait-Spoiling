9959b9623314a8ec8a2ffa55a849c86b65184c1f
Dorothea MacPhail <dorothea.macphail@uni-potsdam.de>
2023-03-25 11:22:02 +0100

    created file that creates and stores the datasets

--- data_storer.py
+++ data_storer.py
@@ -1,8 +1,15 @@
 import numpy as np
+import pandas as pd
 
 from divide_data import divide_x, divide_y, divide_y_one_class
 
 def creata_data_arrays():
+    """
+    A function that takes the original datasets (split into train/val/test) and creates the datasets for the classifier.
+    The dataset for the classifier uses the manually designed features.
+    The final datasets are stored as csv files in Data_Arrays folder.
+    main.py reads from these csv files.
+    """
     print("train data")
     with open('./Data/training_data.jsonl', 'r') as training_data_file:
         data = list(training_data_file)
@@ -26,34 +33,43 @@ def creata_data_arrays():
     y_test_passage = divide_y_one_class(data, ['passage'])
     y_test_multi = divide_y_one_class(data, ['multi'])
 
-    
-    # save the data as numpy objects
-    with open('./Data_Arrays/X_train.npy', 'wb') as f1:
-        np.save(f1, X_train)
+    print("X_train:", X_train.shape)
+    print("y_train:", y_train.shape)
+    print("X_validation:", X_validation.shape)
+    print("y_validation:", y_validation.shape)
+    print("X_test:", X_test.shape)
+    print("y_test:", y_test.shape)
+    print("y_test_phrase:", y_test_phrase.shape)
+    print("y_test_passage:", y_test_passage.shape)
+    print("y_test_multi:", y_test_multi.shape)
 
-    with open('./Data_Arrays/y_train.npy', 'wb') as f2:
-        np.save(f2, y_train)
+    # save data as csv
+    df_X_train = pd.DataFrame(X_train)
+    df_X_train.to_csv("./Data_Arrays/X_train.csv", index=False)
 
-    with open('./Data_Arrays/X_validation.npy', 'wb') as f3:
-        np.save(f3, X_validation)
+    df_y_train = pd.DataFrame(y_train)
+    df_y_train.to_csv("./Data_Arrays/y_train.csv", index=False)
 
-    with open('./Data_Arrays/y_validation.npy', 'wb') as f4:
-        np.save(f4, y_validation)
+    df_X_validation = pd.DataFrame(X_validation)
+    df_X_validation.to_csv("./Data_Arrays/X_validation.csv", index=False)
 
-    with open('./Data_Arrays/X_test.npy', 'wb') as f5:
-        np.save(f5, X_test)
+    df_y_validation= pd.DataFrame(y_validation)
+    df_y_validation.to_csv("./Data_Arrays/y_validation.csv", index=False)
 
-    with open('./Data_Arrays/y_test.npy', 'wb') as f6:
-        np.save(f6, y_test)
+    df_X_test = pd.DataFrame(X_test)
+    df_X_test.to_csv("./Data_Arrays/X_test.csv", index=False)
 
-    with open('./Data_Arrays/y_test_phrase.npy', 'wb') as f7:
-        np.save(f7, y_test_phrase)
+    df_y_test = pd.DataFrame(y_test)
+    df_y_test.to_csv("./Data_Arrays/y_test.csv", index=False)
 
-    with open('./Data_Arrays/y_test_passage.npy', 'wb') as f8:
-        np.save(f8, y_test_passage)
-    
-    with open('./Data_Arrays/y_test_multi.npy', 'wb') as f9:
-        np.save(f9, y_test_multi)
+    df_y_test_phrase = pd.DataFrame(y_test_phrase)
+    df_y_test_phrase.to_csv("./Data_Arrays/y_test_phrase.csv", index=False)
+
+    df_y_test_passage = pd.DataFrame(y_test_passage)
+    df_y_test_passage.to_csv("./Data_Arrays/y_test_passage.csv", index=False)
+
+    df_y_test_multi = pd.DataFrame(y_test_multi)
+    df_y_test_multi.to_csv("./Data_Arrays/y_test_multi.csv", index=False)
 
 if __name__ == "__main__":
     creata_data_arrays()
